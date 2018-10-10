81ec2b86fc25d01ac01bd2b214791f87b2795b62
JoyceAja <ajagbe.joyce@gmail.com>
2018-10-10 14:29:47 -0400

    added a different solution, one without using an extra data structure

--- Strings/isUnique.js
+++ Strings/isUnique.js
@@ -5,17 +5,37 @@
  */
 
  const isUnique = (str) => {
-     const set = new Set([])
+     if(str.length === 0 || str.length === 1) return true;
+
+     const set = new Set([]);
 
      for(let i=0; i< str.length; i++){
-         let char = str[i]
+         let char = str[i];
 
-         if(set.has(char)) return false
-         set.add(char)
+         if(set.has(char)) return false;
+         set.add(char);
      }
      return true;
  }
 
  /**
   * In the case where we cant use an additional data structure
+  * Example "hello" "yay"
+  * I would use two pointers
   */
+
+  const isUnique2 = (str) => {
+      let p1 = 0;
+      let p2 = p1 + 1;
+
+      while(p1 < str.length){
+          while(p2 < str.length){
+            if(str[p1] === str[p2]) return false;
+              p2++;
+          }
+          p1++;
+          p2 = p1;
+
+      }
+      return true;
+  }
