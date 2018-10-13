99ec18ec4f2d3330cb6ee0d4cb51d6e883f40f46
JoyceAja <ajagbe.joyce@gmail.com>
2018-10-13 15:09:09 -0400

    tried a different approach

--- Strings/repeatedString.js
+++ Strings/repeatedString.js
@@ -23,6 +23,9 @@
  * Use a while loop for that iteration
  * Use two pointers and my conditional statement is OR
  * 
+ * I think to have a break through with this problem is discovering how to find substring
+ * Because essentially that is what we are doing except we are also finding a new substring in 
+ * new string we are creating by adding on string A
  */
 
  const repeatedString = (A, B) => {
@@ -57,3 +60,45 @@
  }
 let A = "abcd", B = "cdabcdab"
 console.log(repeatedString(A,B))
+
+// let C = "abcdabcdabcd", D = "cdabcdab"
+// const findingSubStr = (C,D) => {
+//     let p3 = 0;
+//     for(let i=0; i< C.length; i++){
+//         while(C[i] === D[p3]){
+//             p3++
+//         }else{
+
+//         }
+//     }
+// }
+var repeatedStringMatch = function(A, B) {
+    let newStr = A;
+    let p1 = A.length - 1;
+    let p2 = B.length - 1;
+    let count = 1;
+   
+   //I need to loop through my B and newStr
+   //Thinking of solving these linearly
+    while(p2 >= 0 && p1 < 1000) {
+        //if my newString is a little short of my string B add A to newStr
+        if(!newStr[p1]){
+            newStr = A + newStr
+            count++
+            p1 = A.length
+            console.log('here p1', p1, newStr[p1], p2, B[p2])
+        }
+        
+        //When to stop my while loop => at the end of string B
+        if(newStr[p1] === B[p2] ){
+            if(p2 === 0) {
+               return count
+            }
+            p1--
+            p2--
+        }else{
+            p1--
+        }
+    }
+   return -1;
+};
